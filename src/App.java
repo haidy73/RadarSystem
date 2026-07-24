3a15eeff15057ea373bf0395fcf96d8e9fd0e86d
Haidy <haidygamal496@gmail.com>
2026-07-24 23:54:27 +0300

    feat: add the main program

--- src/App.java
+++ src/App.java
@@ -1,5 +1,42 @@
+import dataReader.JsonRadarDataReader;
+import dataReader.RadarDataReader;
+import java.util.Scanner;
+import service.RadarSystem;
+import service.ViolationHandler;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        RadarDataReader reader = new JsonRadarDataReader();
+        ViolationHandler violationHandler = new ViolationHandler();
+        
+        // Create the radar system
+        RadarSystem radarSystem = new RadarSystem(reader, violationHandler);
+        
+        // Process the radar data file
+        String filePath = "data/radarData.json";
+        radarSystem.processRadarData(filePath);
+        
+
+        radarSystem.showMenu();
+
+        try (Scanner scanner = new java.util.Scanner(System.in)) {
+            boolean exit = false;
+            while (!exit) {
+                System.out.print("Enter your choice (1-3): ");
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1 -> radarSystem.getAllFines();
+                    case 2 -> radarSystem.getAllViolationsCount();
+                    case 3 -> {
+                        exit = true;
+                        System.out.println("Exiting the program.");
+                    }
+                    default -> System.out.println("Invalid choice. Please try again.");
+                }
+            }
+            
+        } catch (Exception e) {
+            
+        }
     }
 }
