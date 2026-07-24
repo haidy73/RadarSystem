8435fc7de4d7ad86956316480f22e00a8a818721
Haidy <haidygamal496@gmail.com>
2026-07-24 23:30:14 +0300

    feat(model) : add the final verion of the violation model

--- src/model/Violation.java
+++ src/model/Violation.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Violation {
+    private String type;
+    private String description;
+    private Integer fineAmount;
+    
+    public Violation() {}
 
+    public String getType() { return type; }
+    public String getDescription() { return description; }
+    public Integer getFineAmount() { return fineAmount; }   
+    
+    public void setType(String type) { this.type = type; }          
+    public void setDescription(String description) { this.description = description; }
+    public void setFineAmount(Integer fineAmount) { this.fineAmount = fineAmount; }
 }
